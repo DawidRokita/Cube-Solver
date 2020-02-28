7f403f1678e2180402686021edef795f586e4060
DawidRokita <dawidrokita11@gmail.com>
2020-02-28 21:28:40 +0100

    Added sending solution via bluetooth

--- app/src/main/java/com/example/kostkav3/MainActivity5.java
+++ app/src/main/java/com/example/kostkav3/MainActivity5.java
@@ -5,10 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity5 extends AppCompatActivity {
 
+    public Button button;
     private TextView textView;
+    public BluetoothAdapter btAdapter;
+    public BluetoothDevice btDevice;
+    public BluetoothSocket btSocket;
+
+    public static final String SERVICE_ID = "00001101-0000-1000-8000-00805f9b34fb"; //SPP UUID //nie trzeba zmieniac
+    public static final String SERVICE_ADDRESS = "04:B1:67:8A:8E:84"; // HC-05 BT ADDRESS
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +40,88 @@ public class MainActivity5 extends AppCompatActivity {
 
         textView.setText(result);
 
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        btDevice = btAdapter.getRemoteDevice(SERVICE_ADDRESS);
+
+        if(btAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth not available", Toast.LENGTH_LONG).show();
+        } else {
+            if(!btAdapter.isEnabled()) {
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, 3);
+            }else {
+                ConnectThread connectThread = new ConnectThread(btDevice);
+                connectThread.start();
+            }
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btSocket != null) {
+
+                    try{
+                        OutputStream out = btSocket.getOutputStream();
+                        out.write((textView.getText().toString() + "\r\n").getBytes());
+                    }catch(IOException e) {
+
+                    }
+
+                }
+            }
+        });
+    }
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket thisSocket;
+        private final BluetoothDevice thisDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
+            thisDevice = device;
+
+            try {
+                tmp = thisDevice.createRfcommSocketToServiceRecord(UUID.fromString(SERVICE_ID));
+            } catch (IOException e) {
+                Log.e("TEST", "Can't connect to service");
+            }
+            thisSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            btAdapter.cancelDiscovery();
+
+            try {
+                thisSocket.connect();
+                Log.d("TESTING", "Connected to shit");
+            } catch (IOException connectException) {
+                try {
+                    thisSocket.close();
+                } catch (IOException closeException) {
+                    Log.e("TEST", "Can't close socket");
+                }
+                return;
+            }
+
+            btSocket = thisSocket;
+
+        }
+        public void cancel() {
+            try {
+                thisSocket.close();
+            } catch (IOException e) {
+                Log.e("TEST", "Can't close socket");
+            }
+        }
+
 
     }
 
     private void setupUIViews(){
         textView = (TextView) findViewById(R.id.textView6);
+        button = (Button) findViewById(R.id.button13);
     }
 
 }
