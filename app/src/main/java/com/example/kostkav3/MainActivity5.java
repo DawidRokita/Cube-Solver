61cfa6161d65a592e2345f99fb2fa0bfc154f85a
Dawid Rokita <49307038+DawidRokita@users.noreply.github.com>
2024-06-22 10:37:05 +0200

    dodanie wyswietlania czasu ukladania kostki

--- app/src/main/java/com/example/kostkav3/MainActivity5.java
+++ app/src/main/java/com/example/kostkav3/MainActivity5.java
@@ -17,6 +17,7 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
@@ -26,11 +27,15 @@ public class MainActivity5 extends AppCompatActivity {
     private String part;
     private String[] cutstr;
     public Button button, lista, polacz;
-    private TextView textView, textView2, textView34;
+    private TextView textView, textView2, textView34, czas_tb;
     public ListView listView;
     public BluetoothAdapter btAdapter;
     public BluetoothDevice btDevice;
     public BluetoothSocket btSocket;
+
+    public double czas_sekundy;
+
+    public int czas_liczba;
 
     public static final String SERVICE_ID = "00001101-0000-1000-8000-00805f9b34fb"; //SPP UUID //nie trzeba zmieniac
     //public static final String SERVICE_ADDRESS = "00:19:10:08:C3:22"; // HC-05 BT ADDRESS TUTAJ JEST ADRES HC-05 D.R.1
@@ -74,49 +79,45 @@ public class MainActivity5 extends AppCompatActivity {
                 default:    break;
             }
         }
-        textView.setText(textView.getText().toString() + "E");
 
+        textView.setText(textView.getText().toString() + "E");
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
 
         implementListeners();
 
+        polacz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listView.setVisibility(View.INVISIBLE); //schowaj liste z urzadzeniami bt
+                licznik = 0; //ustaw licznik na 0 aby mozna bylo otworzyc liste bt
 
-
-        //btDevice = btAdapter.getRemoteDevice(SERVICE_ADDRESS);
-
-
-         polacz.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 btDevice = btAdapter.getRemoteDevice(textView34.getText().toString());
-                 if(btAdapter == null) {
-                     Toast.makeText(getApplicationContext(), "Bluetooth not available", Toast.LENGTH_LONG).show();
-                 } else {
-                     if(!btAdapter.isEnabled()) {
-                         Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                         startActivityForResult(enableIntent, 3);
-                     }else {
-                         ConnectThread connectThread = new ConnectThread(btDevice);
-                         connectThread.start();
-                     }
-                 }
-             }
-         });
+                btDevice = btAdapter.getRemoteDevice(textView34.getText().toString());
+                if(btAdapter == null) {
+                    Toast.makeText(getApplicationContext(), "Bluetooth not available", Toast.LENGTH_LONG).show();
+                } else {
+                    if(!btAdapter.isEnabled()) {
+                        Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableIntent, 3);
+                    }else {
+                        ConnectThread connectThread = new ConnectThread(btDevice);
+                        connectThread.start();
+                    }
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(btSocket != null) {
-
                     try{
                         OutputStream out = btSocket.getOutputStream();
                         out.write((textView.getText().toString() + "\r\n").getBytes());
                         Toast.makeText(MainActivity5.this, "Wysłano", Toast.LENGTH_SHORT).show();
                     }catch(IOException e) {
-                        //Toast.makeText(MainActivity5.this, "Błąd wysyłania", Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity5.this, "Błąd wysyłania", Toast.LENGTH_LONG).show();
                     }
-
                 }
             }
         });
@@ -124,7 +125,6 @@ public class MainActivity5 extends AppCompatActivity {
 
     int licznik = 0;
     private void implementListeners() {
-
         lista.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -147,7 +147,6 @@ public class MainActivity5 extends AppCompatActivity {
                     }
                     ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_selectable_list_item, strings);
                     listView.setAdapter(arrayAdapter);
-
                 }
             }
         });
@@ -171,9 +170,8 @@ public class MainActivity5 extends AppCompatActivity {
             thisDevice = device;
             try {
                 tmp = thisDevice.createRfcommSocketToServiceRecord(UUID.fromString(SERVICE_ID));
-
             } catch (IOException e) {
-                Log.e("TEST", "Can't connect to service");
+                Log.e("TEST", "Nie da sie polaczyc");
             }
             thisSocket = tmp;
 
@@ -181,12 +179,16 @@ public class MainActivity5 extends AppCompatActivity {
         }
 
         public void run() {
-            // Cancel discovery because it otherwise slows down the connection.
+
             btAdapter.cancelDiscovery();
 
             try {
                 thisSocket.connect();
-                Log.d("TESTING", "Connected to shit");
+                Log.d("TESTING", "Connected");
+                btSocket = thisSocket;
+                // Start the data receiving thread
+                ReceiveThread receiveThread = new ReceiveThread(btSocket);
+                receiveThread.start();
             } catch (IOException connectException) {
                 try {
                     thisSocket.close();
@@ -195,27 +197,72 @@ public class MainActivity5 extends AppCompatActivity {
                 }
                 return;
             }
-
-            btSocket = thisSocket;
-
         }
-        public void cancel() {
+    }
+
+    private class ReceiveThread extends Thread {
+        private final BluetoothSocket socket;
+        private final InputStream inStream;
+
+        public ReceiveThread(BluetoothSocket socket) {
+            this.socket = socket;
+            InputStream tmpIn = null;
+
             try {
-                thisSocket.close();
+                tmpIn = socket.getInputStream();
             } catch (IOException e) {
-                Log.e("TEST", "Can't close socket");
+                Log.e("TEST", "Error occurred when creating input stream", e);
+            }
+            inStream = tmpIn;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[2]; // Buffor dla odebranych bajtow
+            int numBytes; // zmienna do ktorej wpisujemy bajty
+
+            while (true) {
+                try {
+                    numBytes = inStream.read(buffer);   // czytaj dane z InputStream
+                    if (numBytes == 2) {  // odbieramy tylko dwa bajty bo robot zawsze wysyla dwa bajty
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                String receivedHexData = bytesToHex(buffer);    // konwersja odebranych bajtow do stringa hex
+                                int receivedDecimalData = Integer.parseInt(receivedHexData, 16);    // konwersja hex do dec
+                                czas_sekundy = receivedDecimalData/305.175781;  //zmiana odebranej liczby pulsow z robota na sekundy
+
+                                // zmiana obliczonego czasu na stinga z czasen z dokladnoscia do dwuch cyfr po pzecinku
+                                String timeInSeconds = String.format("%.2f", czas_sekundy);
+                                czas_tb.setText(timeInSeconds + " s");  // wyswietl czas w textView
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    Log.d("TEST", "Rozlaczono ", e);
+                    break;
+                }
             }
         }
+
+        // funkcja zamieniajaca bajty na hex
+        private String bytesToHex(byte[] bytes) {
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02X", b));
+            }
+            return sb.toString();
+        }
+
     }
 
     private void setupUIViews(){
         textView = (TextView) findViewById(R.id.textView6);
         textView2 = (TextView) findViewById(R.id.textView31);
         textView34 = (TextView) findViewById(R.id.textView32);
+        czas_tb = (TextView) findViewById(R.id.czas_tb);
         button = (Button) findViewById(R.id.button13);
         lista = (Button) findViewById(R.id.lista);
         polacz = (Button) findViewById(R.id.button15);
         listView = (ListView) findViewById(R.id.ListView);
     }
-
 }
